c810fe3707ddc5890fb914d3f6263c8105b2b31f
Aaron Diable <aaron.diable@gmail.com>
2020-02-04 10:47:05 +0000

    Refactored some routes

--- polls/urls.py
+++ polls/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.urls import path
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:question_id>/', views.detail, name='detail'),
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,5 @@
 # Modules
-from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 # from django.template import loader
 from .models import Question
@@ -15,13 +14,8 @@ def index(req):
 
 
 def detail(req, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404('Im really sorry, I tried looking for that question, but it simply does not exist!')
-    return render(req, 'polls/details.html', {
-        'question': question
-    })
+    question = get_object_or_404(Question, pk=question_id)
+    return render(req, 'polls/details.html', {'question': question})
 
 
 def results(res, question_id):
